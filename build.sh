35c062bcdf5f28596becb7b5a18762653da66ca2
Adori4nu <53657830+Adori4nu@users.noreply.github.com>
2025-03-25 19:43:18 +0100

    fixing bash build scrpit issue

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-libs= user32.lib gdi32.lib
-cl /EHsc /Ox /nologo /std:c++latest main.cpp /Fertweekend_cl /link$libs
+libs="user32.lib gdi32.lib"
+cl /EHsc /Ox /nologo /std:c++latest main.cpp /Fertweekend_cl /link $libs
